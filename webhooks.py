bbc21975a5319a99fbb32a7fd3e41622e0ce62b8
Olie <olie@obsoletelabs.net>
2025-12-23 21:41:20 +0800

    move files to facilitate more notification methods

---
+++ webhooks.py
@@ -0,0 +1,20 @@
+"""A library for sending notifications via diffrent webhooks"""
+
+import requests
+
+def discord(webhook_url, message_content, username="IP notifier"):
+    """Discord webhook sender"""
+    message = {
+    "username" : username,
+    "content" : message_content
+    }
+
+    result = requests.post(webhook_url, json = message, timeout=3)
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print(f"Payload delivered successfully, code {result.status_code}.")
+        return
