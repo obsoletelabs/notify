1c09386866981df942debcb7d008e4c60a8087e2
Hotzings <hotzings@gmail.com>
2025-12-23 16:59:51 -0500

    Corrected spelling issue of exernal_notifiers instead of external_notifiers added pylint config file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""__init__.py for notify library"""
+from importlib.metadata import version
+
+from .send_email_notification import send_email_via_smtp
+
+
+__version__ = version("my-lib")
+__all__ = ["send_email_via_smtp"]
