de735ec13204c220dc998bd225dc013c4caba8e6
Hotzings <hotzings@gmail.com>
2025-12-23 19:09:32 -0500

    fixes #11

--- webhooks.py
+++ webhooks.py
@@ -18,3 +18,20 @@ def discord(webhook_url, message_content, username="IP notifier"):
     else:
         print(f"Payload delivered successfully, code {result.status_code}.")
         return
+
+# i dont know why you wanted this but its here now
+def generic(webhook_url, message_content):
+    """Webhook sender"""
+    message = {
+    "content" : message_content
+    }
+
+    result = requests.post(webhook_url, json = message, timeout=3)
+
+    try:
+        result.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print(f"Payload delivered successfully, code {result.status_code}.")
+        return
